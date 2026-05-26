26eef3489162bfe5e40af2ff97d3ba476f2bb1b9
Iconcept <iconcept@Parth-s-Macbook-Air.local>
2026-05-26 14:53:17 +0530

    Fix Railway allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-1(&kx5@h9rfqc+2#t@=s18a%pf$jb-wo(y^6otardrc55y35-$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",
+    "localhost",
+    "enterprise-esg-platform-production.up.railway.app",]
 
 
 # Application definition
