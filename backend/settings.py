21c38dc713cdcc140cb0f773a837bfe74b73bd1e
Iconcept <iconcept@Parth-s-Macbook-Air.local>
2026-05-26 15:00:25 +0530

    Fix CSRF settings for production

--- backend/settings.py
+++ backend/settings.py
@@ -23,11 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(&kx5@h9rfqc+2#t@=s18a%pf$jb-wo(y^6otardrc55y35-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1",
     "localhost",
     "enterprise-esg-platform-production.up.railway.app",]
+    
+CSRF_TRUSTED_ORIGINS = [
+    "https://enterprise-esg-platform-production.up.railway.app"
+]
 
 
 # Application definition
