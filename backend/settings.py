7a5e1bb40bc49d2355dbedd3c270c2d9d5116f11
Iconcept <iconcept@Parth-s-Macbook-Air.local>
2026-05-26 15:02:57 +0530

    Production config fix

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-1(&kx5@h9rfqc+2#t@=s18a%pf$jb-wo(y^6otardrc55y35-$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1",
-    "localhost",
-    "enterprise-esg-platform-production.up.railway.app",]
+ALLOWED_HOSTS = ["*"]
     
 CSRF_TRUSTED_ORIGINS = [
     "https://enterprise-esg-platform-production.up.railway.app"
